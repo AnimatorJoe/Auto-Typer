d21f576b497203457bec658b37b102bf3149d6f8
AnimatorJoe <josephjin7@gmail.com>
2020-07-19 14:46:19 -0500

    keep original window open

--- src/main/java/com.animatorjoe/Transcriber.java
+++ src/main/java/com.animatorjoe/Transcriber.java
@@ -1,6 +1,7 @@
 package com.animatorjoe;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,7 +29,7 @@ class Transcriber {
             StringBuilder str = new StringBuilder();
             List<WebElement> elements = driver.findElement(By.className("Prompt")).findElements(By.className("Character"));
             for (WebElement e : elements) { str.append(e.getText()); }
-            System.out.printf("Typing Input: %s%n", str);
+            System.out.printf("Typing Input: %s%n%n", str);
 
             WebElement outputBox = driver.findElement(By.className("InputField"));
             outputBox.click();
@@ -36,15 +37,11 @@ class Transcriber {
 
             // celebrate
             System.out.printf("Wow look at you, you typed at %s words per minute.%n", driver.findElement(By.className("Indicator")).getText().split(": ")[1]);
-            TimeUnit.SECONDS.sleep(5);
-            driver.get("https://youtu.be/6Dh-RL__uN4?t=38");
+            TimeUnit.SECONDS.sleep(3);
+            ((JavascriptExecutor)driver).executeScript("window.open(\"https://youtu.be/6Dh-RL__uN4?t=38\")");
 
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
-        } finally {
-            // quit
-            TimeUnit.SECONDS.sleep(5);
-//            driver.quit();
         }
 
     }
