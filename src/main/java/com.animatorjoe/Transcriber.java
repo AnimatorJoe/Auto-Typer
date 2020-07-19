77910b2c83ab189c18a807de3c529e9a5a939bd9
AnimatorJoe <josephjin7@gmail.com>
2020-07-19 13:21:38 -0500

    typing like a boss

--- src/main/java/com.animatorjoe/Transcriber.java
+++ src/main/java/com.animatorjoe/Transcriber.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 class Transcriber {
     public static void main(String[] args) throws Exception {
         System.out.println("Let's get ready");
 
-        WebDriver driver = new ChromeDriver();;
+        WebDriver driver = new ChromeDriver();
 
         try {
             // load page
@@ -23,14 +24,27 @@ class Transcriber {
                     .until(d -> d.findElement(By.xpath("//button[contains(text(),'Single Player')]")));
             enter.click();
 
+            // read input and write output
+            StringBuilder str = new StringBuilder();
+            List<WebElement> elements = driver.findElement(By.className("Prompt")).findElements(By.className("Character"));
+            for (WebElement e : elements) { str.append(e.getText()); }
+            System.out.printf("Typing Input: %s%n", str);
 
+            WebElement outputBox = driver.findElement(By.className("InputField"));
+            outputBox.click();
+            outputBox.sendKeys(str.toString());
+
+            // celebrate
+            System.out.printf("Wow look at you, you typed at %s words per minute.%n", driver.findElement(By.className("Indicator")).getText().split(": ")[1]);
+            TimeUnit.SECONDS.sleep(5);
+            driver.get("https://youtu.be/6Dh-RL__uN4?t=38");
 
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         } finally {
             // quit
-            TimeUnit.SECONDS.sleep(2);
-            driver.quit();
+            TimeUnit.SECONDS.sleep(5);
+//            driver.quit();
         }
 
     }
